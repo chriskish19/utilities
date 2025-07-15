86ff0cbe3889d0a33665db7472a7d73718155c84
chriskish19 <68289629+chriskish19@users.noreply.github.com>
2025-07-15 09:51:24 -0600

    sdl starter

---
+++ city/projects/depends/macro/names.hpp
@@ -0,0 +1,15 @@
+/**********************************************************/
+//
+// File: names.hpp
+//
+// Purpose: easy file move/name changes in one location 
+//
+// Project: city.core
+//
+/**********************************************************/
+
+#pragma once
+
+// dependencies
+#define CORE_STL_PRIVATE_INCLUDE_PATH			CORE_stl_includes_hpp				// the entire stl library
+#define CORE_STL_INCLUDE_PATH					CORE_stl_hpp						// specific version include stl library
---
+++ sdl_starter/projects/depends/stl/stl_includes.cpp
@@ -0,0 +1,11 @@
+/***************************************
+*  File: stl_includes.cpp (c++ standard 
+*		 library includes)
+*
+*  Purpose: compiled unit
+*
+*  Project: core
+* *************************************/
+
+#include CORE_NAMES_INCLUDE
+#include CORE_STL_PRIVATE_INCLUDE_PATH
